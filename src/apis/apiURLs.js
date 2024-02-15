04dc441ed0f67747cddbbdfcee9b8bc67231b6f9
LeeJinIe <jj074567@gmail.com>
2024-02-15 22:24:37 +0900

    HOTFIX: domain 연결 수정

--- src/apis/apiURLs.js
+++ src/apis/apiURLs.js
@@ -1,6 +1,8 @@
-export const postUrl = `https://dailytopia2.shop/api/posts`;
-export const promotionUrl = `https://dailytopia2.shop/api/promotions`;
-export const reviewUrl = `https://dailytopia2.shop/api/reviews`;
-export const presignedUrl = `https://dailytopia2.shop/api/presigned-urls`;
-export const userUrl = `https://dailytopia2.shop/api/users`;
-export const commentUrl = `https://dailytopia2.shop/api/comments`;
+const root = "/api";
+
+export const postUrl = `${root}/posts`;
+export const promotionUrl = `${root}/promotions`;
+export const reviewUrl = `${root}/reviews`;
+export const presignedUrl = `${root}/presigned-urls`;
+export const userUrl = `${root}/users`;
+export const commentUrl = `${root}/comments`;
