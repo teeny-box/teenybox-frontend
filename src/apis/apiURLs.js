8178b631ae8c92178f6b9c8e22ac05ecdaa81dd7
LeeJinIe <jj074567@gmail.com>
2024-02-20 02:47:59 +0900

    add apiURLs.js

---
+++ src/apis/apiURLs.js
@@ -0,0 +1,11 @@
+const root = "/api";
+
+export const postUrl = `${root}/posts`;
+export const promotionUrl = `${root}/promotions`;
+export const reviewUrl = `${root}/reviews`;
+export const presignedUrl = `${root}/presigned-urls`;
+export const userUrl = `${root}/users`;
+export const commentUrl = `${root}/comments`;
+export const showUrl = `${root}/shows`;
+export const dibsUrl = `${root}/users/bookmarks`;
+export const nicknameUrl = `${root}/users/nickname`;
