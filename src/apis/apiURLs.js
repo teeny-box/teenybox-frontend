1a5116ac0e0d192b1f9f44adb96039d90e6ef6f7
YuBeen So <99963066+kanujoa@users.noreply.github.com>
2024-02-21 19:40:45 +0900

    Update apiURLs.js
    api url root 수정

--- src/apis/apiURLs.js
+++ src/apis/apiURLs.js
@@ -1,5 +1,4 @@
-// const root = "https://teeny-box.com/api";
-const root = "https://dailytopia2.shop/api";
+const root = "/api";
 
 export const postUrl = `${root}/posts`;
 export const promotionUrl = `${root}/promotions`;
