7ddb3c12f82c32c83bd42a5cfd48e147b3dd1050
woo7i <or2447221@gmail.com>
2024-06-03 22:28:02 +0900

    Desgin(PlayDetail) : 상세 연극 정보 ui 수정 및 컴포넌트 수정.

--- src/components/play-list/PlayBox.jsx
+++ src/components/play-list/PlayBox.jsx
@@ -1,40 +1,37 @@
 import React from "react";
-import "./PlayBox.scss";
 import { useNavigate } from "react-router-dom";
+import "./PlayBox.scss";
 
 // 연극 리스트 페이지에 띄울 연극 박스 하나 (포스터, 연극 제목, 열리는 장소, 상영 기간, 대표 가격 이렇게 한 세트)
 export default function PlayBox({ playInfo, query, setPrevPlayListQuery }) {
   const nav = useNavigate();
   const { playId, imgSrc, title, place, period, price, state } = playInfo;
 
+  const handleClick = () => {
+    setPrevPlayListQuery(query);
+    nav(`/play/${playId}?tab=detail-info`, { state: { from: query } });
+  };
+
   return (
-    <>
-      <div
-        className="play-box"
-        onClick={() => {
-          setPrevPlayListQuery(query);
-          nav(`/play/${playId}?tab=detail-info`);
-        }}
-      >
-        <div className="play-img-box">
-          {state === "공연완료" && (
-            <div className="end-show-design">
-              <img src="/banner.png" alt="공연 완료 이미지" />
-              <span className="end-show-text">
-                연극 <br />
-                종료
-              </span>
-            </div>
-          )}
-          <img src={imgSrc} alt={`${title} 포스터`} />
-        </div>
-        <div className="play-details">
-          <div className="play-title">{title.length >= 28 ? `${title.slice(0, 27)}...` : title}</div>
-          <div className="play-place">{place.length >= 17 ? `${place.slice(0, 16)}...` : place}</div>
-          <div className="play-period">{period}</div>
-          <div className="play-price">{price.length >= 40 ? `${price.slice(0, 40)}...` : price}</div>
-        </div>
+    <div className="play-box" onClick={handleClick}>
+      <div className="play-img-box">
+        {state === "공연완료" && (
+          <div className="end-show-design">
+            <img src="/banner.png" alt="공연 완료 이미지" />
+            <span className="end-show-text">
+              연극 <br />
+              종료
+            </span>
+          </div>
+        )}
+        <img src={imgSrc} alt={`${title} 포스터`} />
       </div>
-    </>
+      <div className="play-details">
+        <div className="play-title">{title.length >= 28 ? `${title.slice(0, 27)}...` : title}</div>
+        <div className="play-place">{place.length >= 17 ? `${place.slice(0, 16)}...` : place}</div>
+        <div className="play-period">{period}</div>
+        <div className="play-price">{price.length >= 40 ? `${price.slice(0, 40)}...` : price}</div>
+      </div>
+    </div>
   );
 }
